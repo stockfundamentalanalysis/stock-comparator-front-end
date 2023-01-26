43544a418777cc6de0785de8cf5748b74278c5cd
Alex Caballero <alex.caballero.4@gmail.com>
2023-01-26 16:49:45 +0100

    Create Next.js project
    Install Next.js, Tailwind CSS, Prettier & ESLint.
    
    Create Next.js project.
    
    Create config for Typescript, Prettier & ESLint.

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Stock Comparator</title>
+        <meta name="description" content="Stock Comparator App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello, World!</h1>
+      </main>
+    </>
+  )
+}

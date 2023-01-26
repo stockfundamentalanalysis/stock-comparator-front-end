3f8289df4794e2810ab8dd7f2e3050084da777c7
Alex Caballero <alex.caballero.4@gmail.com>
2023-01-26 16:56:46 +0100

    Add Tailwind CSS
    Create config for Tailwind CSS.
    
    Import CSS files in _app.
    
    Add test style to H1 in index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello, World!</h1>
+        <h1 className="text-3xl font-bold underline">Hello, World!</h1>
       </main>
     </>
   )

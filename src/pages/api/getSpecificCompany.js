3b2a93cefdad2f358477a9167e6992da79d08423
martinpcs21 <codepcs21@gmail.com>
2023-12-07 18:19:23 +0100

    Company detail updated

---
+++ src/pages/api/getSpecificCompany.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module'
+import { PrismaClient } from '@prisma/client'
+
+const require = createRequire(import.meta.url)
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  const {
+    query: { ticker },
+  } = req
+  try {
+    const companyAnalysis = await prisma.simpleAnalysis.findFirst({
+      where: {
+        Ticker: ticker,
+      },
+    })
+
+    const companyVariables = await prisma.Companies.findFirst({
+      where: {
+        Ticker: ticker,
+      },
+    })
+
+    // Combine the values of both objects into a new object
+    const companyCombinedData = {
+      ...companyAnalysis,
+      ...companyVariables,
+    }
+
+    res.status(200).json(companyCombinedData)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    res.status(500).json({ error: 'An error occurred while fetching data' })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
--- src/pages/detail/[companyDetail].tsx
+++ src/pages/detail/[companyDetail].tsx
@@ -2,7 +2,6 @@ import React, { useMemo, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Radar from '../../components/radar'
 import Navbar from '@/components/navbar'
-import json from '../../data/sfa_easy.json'
 import {
   Box,
   FormControl,
@@ -14,7 +13,20 @@ import {
   SelectChangeEvent,
   Stack,
   Chip,
+  CircularProgress,
 } from '@mui/material'
+
+interface CompanyData {
+  CompanyName: string
+  Sector: string
+  TargetPrice: number
+  Potential: number
+  GrowthScore: any
+  DebtQualityScore: any
+  EarningsScore: any
+  ProfitabilityScore: any
+  // Add other properties as needed
+}
 
 const Post = () => {
   //Get the current path in company detail page
@@ -22,16 +34,38 @@ const Post = () => {
   const cleanPath = router.asPath.split('#')[0].split('?')[0].split('l/')[1]
   const company_name = cleanPath.toUpperCase()
   //Filter the json data to get the company detail
-  const data = Object.values(json)
-  const company = data.filter((item) => item.Ticker === company_name)[0]
-  const tickers = data.map((item) => item.Ticker)
-  ///console.log(String(company_name))
-  let color = 'text.primary'
-  if (company.Potential > 0.1) {
-    color = 'success.main'
-  } else if (company.Potential < -0.1) {
-    color = 'error.main'
-  }
+  const [isLoading, setIsLoading] = useState(true) // Add loading state
+  const [company, setCompanyData] = useState<CompanyData | null>(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `/api/getSpecificCompany?ticker=${company_name}`
+        )
+        if (!response.ok) {
+          throw new Error('Failed to fetch company data')
+        }
+        const company = await response.json()
+        //const company = Object.values(dataJson)
+        //const company = data.filter((item) => item.Ticker === company_name)[0]
+        setCompanyData(company)
+        // const tickers = data.map((item) => item.Ticker)
+        ///console.log(String(company_name))
+        let color = 'text.primary'
+        if (company.Potential > 0.1) {
+          color = 'success.main'
+        } else if (company.Potential < -0.1) {
+          color = 'error.main'
+        }
+      } catch (error) {
+        console.error('Error fetching company data:', error)
+      } finally {
+        setIsLoading(false) // Set loading state to false when done
+      }
+    }
+
+    fetchData()
+  }, [])
 
   const ITEM_HEIGHT = 48
   const ITEM_PADDING_TOP = 8
@@ -59,112 +93,111 @@ const Post = () => {
   return (
     <>
       <Navbar></Navbar>
-      <Grid container spacing={3}>
-        <Grid item md>
-          <Stack spacing={2} sx={{ alignItems: 'center' }}>
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                boxShadow: 1,
-                borderRadius: 2,
-                p: 2,
-                minWidth: 300,
-                maxWidth: 300,
-              }}
-            >
-              <Box sx={{ color: 'text.secondary' }}>Company:</Box>
+      {isLoading ? (
+        // Display a loading indicator while fetching data
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={3}>
+          <Grid item md>
+            <Stack spacing={2} sx={{ alignItems: 'center' }}>
               <Box
                 sx={{
-                  color: 'black',
-                  fontSize: 24,
-                  fontWeight: 'medium',
+                  bgcolor: 'background.paper',
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                  maxWidth: 300,
                 }}
               >
-                {company.CompanyName}
+                <Box sx={{ color: 'text.secondary' }}>Company:</Box>
+                <Box
+                  sx={{
+                    color: 'black',
+                    fontSize: 24,
+                    fontWeight: 'medium',
+                  }}
+                >
+                  {company.CompanyName}
+                </Box>
               </Box>
-            </Box>
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                boxShadow: 1,
-                borderRadius: 2,
-                p: 2,
-                minWidth: 300,
-                maxWidth: 300,
-              }}
-            >
-              <Box sx={{ color: 'text.secondary' }}>Sector:</Box>
               <Box
                 sx={{
-                  color: 'black',
-                  fontSize: 18,
-                  fontWeight: 'medium',
+                  bgcolor: 'background.paper',
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                  maxWidth: 300,
                 }}
               >
-                {company.Sector}
+                <Box sx={{ color: 'text.secondary' }}>Sector:</Box>
+                <Box
+                  sx={{
+                    color: 'black',
+                    fontSize: 18,
+                    fontWeight: 'medium',
+                  }}
+                >
+                  {company.Sector}
+                </Box>
               </Box>
-            </Box>
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                boxShadow: 1,
-                borderRadius: 2,
-                p: 2,
-                minWidth: 300,
-                maxWidth: 300,
-              }}
-            >
-              <Box sx={{ color: 'text.secondary' }}>TargetPrice:</Box>
               <Box
                 sx={{
-                  color: 'black',
-                  fontSize: 24,
-                  fontWeight: 'medium',
+                  bgcolor: 'background.paper',
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                  maxWidth: 300,
                 }}
               >
-                {Math.round(company.TargetPrice * 10) / 10}
+                <Box sx={{ color: 'text.secondary' }}>TargetPrice:</Box>
+                <Box
+                  sx={{
+                    color: 'black',
+                    fontSize: 24,
+                    fontWeight: 'medium',
+                  }}
+                >
+                  {Math.round(company.TargetPrice * 10) / 10}
+                </Box>
               </Box>
-            </Box>
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                boxShadow: 1,
-                borderRadius: 2,
-                p: 2,
-                minWidth: 300,
-                maxWidth: 300,
-              }}
-            >
-              <Box sx={{ color: 'text.secondary' }}>Potential:</Box>
               <Box
                 sx={{
-                  color:
-                    company.Potential > 0.3
-                      ? 'green'
-                      : company.Potential >= -0.1
-                      ? 'black'
-                      : 'red',
-                  fontSize: 34,
-                  fontWeight: 'medium',
+                  bgcolor: 'background.paper',
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                  maxWidth: 300,
                 }}
               >
-                {Math.round(company.Potential * 100)} %
+                <Box sx={{ color: 'text.secondary' }}>Potential:</Box>
+                <Box
+                  sx={{
+                    color:
+                      company.Potential > 0.3
+                        ? 'green'
+                        : company.Potential >= -0.1
+                        ? 'black'
+                        : 'red',
+                    fontSize: 34,
+                    fontWeight: 'medium',
+                  }}
+                >
+                  {Math.round(company.Potential * 100)} %
+                </Box>
               </Box>
-            </Box>
-          </Stack>
+            </Stack>
+          </Grid>
+          <Grid item md>
+            <Radar company={company}></Radar>
+          </Grid>
         </Grid>
-        <Grid item md>
-          <Radar company={company}></Radar>
-        </Grid>
-      </Grid>
+      )}
     </>
   )
-}
-
-Post.getInitialProps = async () => {
-  const res = await import('../../data/sfa_easy.json')
-  const data = Object.values(res)
-  return { props: { data } }
 }
 
 export default Post
